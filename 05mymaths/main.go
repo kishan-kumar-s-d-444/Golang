921a11f39cbf07481f31034a4439bfa9a21efb49
Kishan Kumar S D <kishankumarsd444@gmail.com>
2025-11-18 17:10:40 +0530

    06

--- 05mymaths/main.go
+++ 05mymaths/main.go
@@ -1,18 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/big"
+	// "math/rand"
+	"crypto/rand"
+)
 
 func main() {
 	fmt.Println("Welcome")
-	var num1 int =2
-	var num2 float64=4
-	fmt.Println("The sum is: ",num1+int(num2))
+	var num1 int = 2
+	var num2 float64 = 4
+	fmt.Println("The sum is: ", num1+int(num2))
 
-	//random number 
-	//two ways
+	//random number two ways
 	//1 mathematics
+	// rand.Seed(time.Now().UnixNano())
+	// fmt.Println(rand.Intn(5))
+
 	//2 cryptography
-
-	
-
+	randnum, _ := rand.Int(rand.Reader, big.NewInt(5))
+	fmt.Println(randnum)
 }
--- 06mytime/main.go
+++ 06mytime/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("TIME PACKAGES")
+
+	presentTime := time.Now()
+	fmt.Println("Current time: ", presentTime)
+	fmt.Println(presentTime.Format("01-02-2006 Monday"))
+
+	createdDate := time.Date(1004, time.August, 2, 23, 23, 0, 0, time.UTC)
+	fmt.Println(createdDate)
+	fmt.Println(createdDate.Format("01-02-2006 Monday"))
+}
